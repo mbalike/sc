52107886cff6939c5f4d06a181c34971f5d7bd4b
mbalike <dmbalike@outlook.com>
2026-05-04 21:56:45 +0300

    new box design

--- test-server.js
+++ test-server.js
@@ -39,7 +39,12 @@ const server = http.createServer((req, res) => {
         res.end('Server Error: ' + error.code);
       }
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
+      res.writeHead(200, {
+        'Content-Type': contentType,
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      });
       res.end(content, 'utf-8');
     }
   });
